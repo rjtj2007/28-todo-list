586db3623673dfd9b4ea898e480b0d56e3cd455a
rjtj2007 <teacherman1@gmail.com>
2018-04-28 14:52:08 -0700

    re-configuring webpack

--- lab-ryan/webpack.config.js
+++ lab-ryan/webpack.config.js
@@ -2,21 +2,14 @@ const HtmlPlugin = require('html-webpack-plugin');
 
 const config = {
   mode: 'development',
-  devtool: 'source-map',
-  entry: `${__dirname}/src/main.js`,
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'bundle-[hash].js',
-  },
+  entry: './src/main.js',
+  plugins: [new HtmlWebpackPlugin({template: __dirname + '/src/index.html'})],
   module: {
     rules: [
       {test: /\.(jsx?$)/, loader: ["babel-loader"], exclude: /node_modules/},
       {test: /\.s?css$/, loader: ["style-loader", "css-loader", "sass-loader"]},
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: `${__dirname}/src/index.html`})
-  ],
+  }
 };
 
 
