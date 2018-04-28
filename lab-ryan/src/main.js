19d9b6bd2d818fa26c6b73e04935c2a8060fcf6e
rjtj2007 <teacherman1@gmail.com>
2018-04-28 12:37:51 -0700

    building functionality of main routes

--- lab-ryan/src/main.js
+++ lab-ryan/src/main.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import {
-//     BrowserRouter as Router,
-//     Route,
-//     Link
-// } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+import Landing from './components/landing.js';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Notes App'
+        }
+    }
+
+    render(){
+        return <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Landing</Link></li>
+                        <li><Link to="/notes">Notes</Link></li>
+                    </ul>
+                </nav>
+                <h1>{this.state.title}</h1>
+                <Route exact path= "/" component={Landing} />
+                <Route path="/notes" component={Notes} />
+            </div>
+        </Router>
+    }
+}
 
 let root = document.createElement('div');
 document.body.appendChild(root);
