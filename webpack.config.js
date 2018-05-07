27a991d61c3db23d6e4a2c4e2a74b0b191dc4c1a
rjtj2007 <teacherman1@gmail.com>
2018-05-07 11:38:23 -0700

    starting refactor

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,24 @@ const HtmlPlugin = require('html-webpack-plugin');
 const config = {
   mode: 'development',
   devtool: 'source-map',
-  entry: `${__dirname}/src/main.jsx`,
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'bundle-[hash].jsx',
-  },  
-  module: {
-    rules: [
-      {test: /\.(jsx?$)/, loader: ["babel-loader"], exclude: /node_modules/},
-      {test: /\.s?css$/, loader: ["style-loader", "css-loader", "sass-loader"]},
-    ]
-  },
+  entry: '/src/main.jsx',
   plugins: [
     new HtmlPlugin({template: __dirname + '/src/index.html'})
-  ],
+  ],  
+  module: {
+    rules: [
+      {test: /\.(jsx?$)/, 
+        loader: ['babel-loader'], 
+        exclude: /node_modules/
+      },
+      {test: /\.s?css$/, 
+        loader: [
+        'style-loader', 
+        'css-loader', 
+        'sass-loader'
+      ]},
+    ]
+  },
 };
 
 
